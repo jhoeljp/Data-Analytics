9048119e180149881f0edb993caa2a6e7183416b
Jhoel Perez Ruiz <jhoel.jjpr@gmail.com>
2022-10-14 16:18:32 +0300

    implemented exceptions for data fetching convervation

--- Music_Charts_Last_fm/last_fm_api.py
+++ Music_Charts_Last_fm/last_fm_api.py
@@ -10,6 +10,60 @@ from time import sleep, time
 import pandas as pd 
 import requests, json
 
+#single api request 
+def last_fm_api(header,payload):
+    end_point = 'https://ws.audioscrobbler.com/2.0/'
+
+    #perform request to host 
+    response = None
+    try:
+        response = requests.get(end_point,headers=header,params=payload)
+    except:
+        return None,404
+
+    #convert json to text
+    obj = response.json()
+    
+    # text = json.dumps(obj, sort_keys=True, indent=4)
+    # data= json.loads(text)
+
+    # if it's not a cached result, sleep
+    if not getattr(response, 'from_cache', False):
+        #delay request for cache request API limit 
+        sleep(0.25)
+
+    return obj, response.status_code
+
+#api paging requests
+def last_fm_api_paging(max_pages,headers,payload):
+    results = [] 
+    page = payload['page']
+
+    while page <= max_pages:
+        #assign page number to payload 
+        payload['page'] = page
+
+        #make request with page info 
+        try:
+            data, status = last_fm_api(headers,payload)
+        except:
+            #if error occurs save fetched data
+            return results
+
+        #if request invalid stop loop 
+        if status != 200:
+            print(f"somethig went wrong: Status {status}")
+            break
+        
+        # print(f"Requesting page {page}/{max_pages}")
+        print(f"{page}/{max_pages}")
+
+        #append request 
+        results.append(data)
+
+        page += 1
+
+    return results
 
 if __name__ == "__main__":
     #load environment variables 
@@ -22,60 +76,6 @@ if __name__ == "__main__":
         API_key = environ.get("API_key")
         API_secret = str(environ.get("API_Shared_secret"))
 
-        #single api request 
-        def last_fm_api(header,payload):
-            end_point = 'https://ws.audioscrobbler.com/2.0/'
-
-            response = requests.get(end_point,headers=header,params=payload)
-
-            #raise exception if request invalid 
-            response.raise_for_status()
-            # print(response.url)
-
-            #convert json to text 
-            obj = response.json()
-            # text = json.dumps(obj, sort_keys=True, indent=4)
-            # data= json.loads(text)
-
-            result = obj
-            # result = data['artists'][attribute]
-
-            # if it's not a cached result, sleep
-            if not getattr(response, 'from_cache', False):
-                sleep(0.25)
-
-            return result, response.status_code
-
-        #api paging requests
-        def last_fm_api_paging(max_pages,headers,payload):
-            results = [] 
-            page =1
-
-
-            while page <= max_pages:
-                #assign page number to payload 
-                payload['page'] = page
-
-                # print(f"Requesting page {page}/{max_pages}")
-                print(f"{page}/{max_pages}")
-
-                #make request with page info 
-                data, status = last_fm_api(headers,payload)
-
-                #if request invalid stop loop 
-                if status != 200:
-                    break
-
-                #append request 
-                results.append(data)
-
-                page += 1
-
-                #delay request for cache request API limit 
-                # sleep(0.25)
-
-            return results
-
         #API request 
         header ={
             'user-agent': 'Dataquest'
@@ -86,32 +86,47 @@ if __name__ == "__main__":
             'method': 'chart.gettopartists',
             'format': 'json',
             'page': 1
-        }
+        } 
 
-        #api request time elapsed 
+        all_data = []
         time_start = time()
 
-        #get max num of pages 
-        pages, status = last_fm_api(header, payload)
-        max_pages = int(pages['artists']['@attr']['totalPages'])
+        try:
+            #get max num of pages 
+            pages, status = last_fm_api(header, payload)
 
-        print(f"total pages: {max_pages}")
+            max_pages = int(pages['artists']['@attr']['totalPages'])
+            # max_pages = 10000
 
-        #make cached requests 
-        all_data = last_fm_api_paging(max_pages,header, payload)
-        # all_data = last_fm_api_paging(max_pages,header, payload,'artists')
+            print(f"total pages: {max_pages}")
 
-        #convert data to Data Frame 
-        frames = [pd.DataFrame(r['artists']['artist']) for r in all_data]
+            #make cached requests 
+            
+            all_data = last_fm_api_paging(max_pages,header, payload)
 
-        #combine all request into one frame
-        artists = pd.concat(frames)
-        
-        #save dataframe to csv 
-        artists.to_csv(f'{getcwd()}\Chart_artists_Lastfm.csv')
+        except ConnectionError as ex:
+            print(f"Oops Error establishing connection!")
 
-        #time elapsed total 
-        print(f"{time()-time_start}")
+        except Exception as ex: 
+            print(f"Oops error has occured! {ex}")
+
+        finally:
+            if all_data != []:
+
+                #convert data to Data Frame 
+                frames = [pd.DataFrame(r['artists']['artist']) for r in all_data]
+
+                #combine all request into one frame
+                artists = pd.concat(frames)
+                
+                #save dataframe to csv 
+                artists.to_csv(f'{getcwd()}\Chart_artists_Lastfm.csv')
+
+            else:
+                print("No Records retrieved! ")
+
+            time_f = (time()-time_start)/60
+            print(f"Time elapsed {round(time_f,2)} minutes! ")
 
     else: 
         print("invalid environment file...")
